1669bbe3fdadf2a6e0364329c8df458db3f0ecf3
yuvraj.soni <yuvraj.soni@ksolves.com>
2024-06-04 11:19:17 +0530

    added list

--- listandbool.py
+++ listandbool.py
@@ -2,3 +2,24 @@ first_name = "yuvraj"
 last_name = "soni"
 print(first_name.isdigit())
 print(True and False)
+a = []
+print(type(a))
+
+for i in 'yuvraj':
+    a.append(i)
+print(a[-2:-5:-1])
+print(a[1:])
+b = (i for i in "yuvraj")
+a.append([x for x in b])
+print(a)
+
+a.extend(b)
+a.extend(['y', 'u'])
+
+print(a)
+print(a.count('y'))
+print("hello")
+print(a*2)
+print(min(a))
+print(max(a))
+print("hello")
